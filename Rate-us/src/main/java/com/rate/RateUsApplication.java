9e6ab1519db9685f6670f6e7d666acb11a4fe929
deepakbsol <kumar.dpk03@gmail.com>
2023-06-29 11:40:17 +0530

    structure of Rate us server

---
+++ Rate-us/src/main/java/com/rate/RateUsApplication.java
@@ -0,0 +1,13 @@
+package com.rate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateUsApplication.class, args);
+	}
+
+}
