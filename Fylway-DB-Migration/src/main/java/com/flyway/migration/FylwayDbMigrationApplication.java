79631fadabf06a98a58c3daeef145363ab520dde
deepakbsol <kumar.dpk03@gmail.com>
2023-06-29 22:55:21 +0530

    central flyway service for db migration

---
+++ Fylway-DB-Migration/src/main/java/com/flyway/migration/FylwayDbMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.flyway.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FylwayDbMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FylwayDbMigrationApplication.class, args);
+	}
+
+}
---
+++ Fylway-DB-Migration/src/test/java/com/flyway/migration/FylwayDbMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyway.migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FylwayDbMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
