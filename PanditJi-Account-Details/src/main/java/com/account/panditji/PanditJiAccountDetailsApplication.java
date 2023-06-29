40e9dc109d2c4d870d165933e8b5305572d00693
deepakbsol <kumar.dpk03@gmail.com>
2023-06-29 11:18:52 +0530

    structure of PanditJi-Account-Details

---
+++ PanditJi-Account-Details/src/main/java/com/account/panditji/PanditJiAccountDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.account.panditji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanditJiAccountDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanditJiAccountDetailsApplication.class, args);
+	}
+
+}
---
+++ PanditJi-Account-Details/src/test/java/com/account/panditji/PanditJiAccountDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.account.panditji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanditJiAccountDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
