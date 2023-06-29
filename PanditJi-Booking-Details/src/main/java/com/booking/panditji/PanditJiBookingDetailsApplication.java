8b44f63e39772293b92799bed943d14873c5b7f6
deepakbsol <kumar.dpk03@gmail.com>
2023-06-29 11:21:50 +0530

    structure of PanditJi-Booking-Details

---
+++ PanditJi-Booking-Details/src/main/java/com/booking/panditji/PanditJiBookingDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.booking.panditji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanditJiBookingDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanditJiBookingDetailsApplication.class, args);
+	}
+
+}
---
+++ PanditJi-Booking-Details/src/test/java/com/booking/panditji/PanditJiBookingDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.panditji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanditJiBookingDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
