af302bbac568db7331c0f87abf9a37a7c0ec480c
deepakbsol <kumar.dpk03@gmail.com>
2023-06-29 11:10:51 +0530

    structure of Function-Details

---
+++ Function-Details/src/main/java/com/function/FunctionDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionDetailsApplication.class, args);
+	}
+
+}
