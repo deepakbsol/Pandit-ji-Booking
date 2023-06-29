b9aab3e7b5f390fbe18f6d38aa7fbf94adc08471
deepakbsol <kumar.dpk03@gmail.com>
2023-06-29 11:37:44 +0530

    structure of PanditJi-Registration

---
+++ PanditJi-Registration/src/main/java/com/register/panditji/PanditJiRegistration/PanditJiRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.register.panditji.PanditJiRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanditJiRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanditJiRegistrationApplication.class, args);
+	}
+
+}
---
+++ PanditJi-Registration/src/test/java/com/register/panditji/PanditJiRegistration/PanditJiRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.register.panditji.PanditJiRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanditJiRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
